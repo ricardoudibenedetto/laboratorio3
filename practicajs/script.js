2116732c0f9a1ab12786f4e58fdbb1a7e323a45e
alumno <alumno@utnfra.com.ar>
2019-09-10 22:16:23 -0300

    js

--- practicajs/script.js
+++ practicajs/script.js
@@ -1,18 +1,24 @@
-function Persona(nombre,apellido,edad){
-    /* nombre: "juan",
+/* function Persona(nombre, apellido, edad) {
+    nombre: "juan",
     apellido: "perex",
-    edad: 30 ,*/
+    edad: 30 ,
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
-    this.saludar = function(){
+    this.saludar = function () {
         return `hola, me llamo ${this.nombre}`;
     }
 };
 
 var p1 = new Persona("jose", "alvarez", 34);
 var p2 = new Persona("ana", "gonzales", 20);
-
+Persona.prototype.altura = 1.50;
 p1.altura = 80;
-console.log( p1.altura);
-console.log( p2.saludar());
+console.log(p1.altura);
+console.log(p2.altura);
+ */
+ var foo = function(a,b,c){
+    console.log(arguments) //arguments[0] , arguments.length , arguments
+ }
+ var x = foo;
+ x(2,3);
