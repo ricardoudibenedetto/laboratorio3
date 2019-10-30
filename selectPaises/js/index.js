37baf397eda5207aba5440012b5d6b35374c7546
alumno <alumno@utnfra.com.ar>
2019-10-29 22:11:43 -0300

    a

---
+++ selectPaises/js/index.js
@@ -0,0 +1,47 @@
+let selPaises;
+let selCiudades;
+
+
+window.addEventListener("load", function () {
+    selPaises = document.getElementById('selPaises');
+    selCiudades = document.getElementById('selCiudades');
+
+    cargarSelect(selPaises,obtenerPaises(datos));
+    cargarSelect(selCiudades,obtenerCiudades(datos, selPaises.value));
+    selPaises.addEventListener("change", (e)=>{
+        cargarSelect(selCiudades,obtenerCiudades(datos, e.target.value));
+        });
+});
+
+function obtenerPaises(arr) {
+    return arr.map(element => element.pais)
+                    .unique()
+                    .sort();
+}
+
+function obtenerCiudades(arr, pais) {
+    return arr.filter(element => element.pais === pais)
+                        .map(element => element.ciudad)
+                         .unique()
+                            .sort();
+}
+
+function cargarSelect(sel, arr) {
+    //selCiudades.options.length = 0;
+    limpiarSelect(selCiudades);
+    arr.forEach(element => {
+        let opcion = document.createElement('option');
+        opcion.setAttribute('value', element);
+        let texto = document.createTextNode(element);
+        opcion.appendChild(texto);
+        sel.appendChild(opcion);
+    });
+}
+function limpiarSelect(sel) {
+    while(sel.hasChildNodes()){
+        sel.remove(sel.firstElementChild);
+    }
+}
+Array.prototype.unique = function () {
+    return [...new Set(this)]
+};
