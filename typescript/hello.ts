a38460dd851368a28b97cda7ce0f47a0f13db026
alumno <alumno@utnfra.com.ar>
2019-11-12 21:59:16 -0300

    typescript

--- typescript/hello.ts
+++ typescript/hello.ts
@@ -1,24 +1,183 @@
 
 
 function holaMundo(monto: number | string): string {
-    let hola : string | any;
+    let hola: string | any;
     return monto.toString();
 }
 
-let mensaje : string;
+let mensaje: string;
 //array
-let array : number[];
+let array: number[];
 //tupla
-let tupla : [number, string, [number, string]] = [1,"lala",[2, "string"]]; 
+let tupla: [number, string, [number, string]] = [1, "lala", [2, "string"]];
 
-enum Eheroe{
+enum Eheroe {
     xmen,
     avenger
 }
 
 
-for(let key in Eheroe) {
+for (let key in Eheroe) {
     console.log(key);
 }
 
+
+let funcionEnviarMision2 = function (...heroes: string[]) {
+    for (let i = 0; i < heroes.length; i++) {
+        console.log(`${heroes[i]} enviado`);
+    }
+}
+
+funcionEnviarMision2("spiderman", "superman", "tuhermanagirl");
+
+//funcion Flecha
+
+let funcionEnviarMision3 = (heroe: string = "Heroe"): string => {
+    return `${heroe} enviado a la mision 3`;
+}
+
+console.log(funcionEnviarMision3());
+
+let flas: { nombre: string, edad: number, poderes: string[], getNombre: () => string } =
+{
+    nombre: "joseEnrrique",
+    edad: 24,
+    poderes: ["corre", "viaja en el roca comodo"],
+    getNombre() {
+        return this.nombre;
+    }
+}
+
+//tipo personalizado 
+type Heroe = {
+    nombre: string,
+    edad: number,
+    poderes?: string[],
+    getNombre: () => string
+};
+
+
+let ironman: Heroe = {
+    nombre: "tony stark",
+    edad: 84,
+    getNombre: function () { return this.nombre }
+};
+
+console.log(ironman.getNombre());
+
+
+//interfaces
+interface IHeroe {
+    nombre: string,
+    poder?: string,
+    mostrar?(): string
+}
+
+let gepardo: IHeroe = {
+    nombre: "James"
+};
+
+console.log(gepardo.nombre);
+
+
+//interfaces en clases 
+class Avenger implements IHeroe {
+    nombre: string = "un avenger";
+
+    constructor(nombre:string) {
+        this.nombre = nombre;
+    }
+}
+
+class Mutante implements IHeroe {
+    nombre: string = "un mutante"
+}
+
+let unAvenger = new Avenger("hulk");
+let unMutante = new Mutante();
+
+console.log(unMutante.nombre);
+
+
+//interace en funcion
+interface IFuncDosNumeros {
+    (num1: number, num2: number): number;
+}
+
+let mifuncion: IFuncDosNumeros;
+mifuncion = (num1, num2) => num1 + num2;
+
+console.log(mifuncion(2,98));
+
+//clases
+let av2 = new Avenger("hombre hormiga");
+console.log(av2.nombre);
+
+class Avenger3 {
+    private _nombre:string = "un av";
+    private _edad : number = 23;
+
+    constructor(nombre:string) {
+        this._nombre = nombre;
+    }
+
+    get edad():number{return this._edad;}
+    set edad(e:number){this._edad = e;}
+
+    public mostrar = ()=>this._nombre;
+
+}
+
+let av3 = new Avenger3("joseEnrique");
+
+av3.edad = 82;
+console.log(av3.mostrar());
+console.log(av3.edad);
+
+
+class Xmen{
+    static nombreDeClase = "Xmen";
+}
+
+console.log(Xmen.nombreDeClase);
+
+//herencia 
+class AvengerHeredado extends Avenger3{
+
+}
+
+let avH = new AvengerHeredado("cocacola");
+
+console.log(avH.mostrar());
+
+//herencia2
+class AvengerHeredado2 extends Avenger3{
+    edad:number = 0;
+    constructor(nombre:string, edad:number){
+        super(nombre)
+        this.edad = edad;
+    }
+}
+
+let av5: AvengerHeredado2 = new AvengerHeredado2("rigoberto",23);
+
+console.log(`Avenger ${av5.mostrar()}, edad: ${av5.edad}`);
+
+//namespaces
+
+namespace Funciones{
+    export function f1(){
+        console.log("soy f1");
+    }
+    export function f2(){
+        console.log("soy f2");
+    }
+}
+
+Funciones.f1();
+Funciones.f2();
+
+$(function(){
+    console.log("ready");
+})
 
--- typescript/output.js
+++ typescript/output.js
@@ -1,15 +1,17 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     };
-    return __assign.apply(this, arguments);
-};
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function holaMundo(monto) {
     var hola;
     return monto.toString();
@@ -27,10 +29,130 @@ var Eheroe;
 for (var key in Eheroe) {
     console.log(key);
 }
-var prueba = __assign({}, Eheroe);
-for (var key in prueba) {
-    console.log(key);
-}
+var funcionEnviarMision2 = function () {
+    var heroes = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        heroes[_i] = arguments[_i];
+    }
+    for (var i = 0; i < heroes.length; i++) {
+        console.log(heroes[i] + " enviado");
+    }
+};
+funcionEnviarMision2("spiderman", "superman", "tuhermanagirl");
+//funcion Flecha
+var funcionEnviarMision3 = function (heroe) {
+    if (heroe === void 0) { heroe = "Heroe"; }
+    return heroe + " enviado a la mision 3";
+};
+console.log(funcionEnviarMision3());
+var flas = {
+    nombre: "joseEnrrique",
+    edad: 24,
+    poderes: ["corre", "viaja en el roca comodo"],
+    getNombre: function () {
+        return this.nombre;
+    }
+};
+var ironman = {
+    nombre: "tony stark",
+    edad: 84,
+    getNombre: function () { return this.nombre; }
+};
+console.log(ironman.getNombre());
+var gepardo = {
+    nombre: "James"
+};
+console.log(gepardo.nombre);
+//interfaces en clases 
+var Avenger = /** @class */ (function () {
+    function Avenger(nombre) {
+        this.nombre = "un avenger";
+        this.nombre = nombre;
+    }
+    return Avenger;
+}());
+var Mutante = /** @class */ (function () {
+    function Mutante() {
+        this.nombre = "un mutante";
+    }
+    return Mutante;
+}());
+var unAvenger = new Avenger("hulk");
+var unMutante = new Mutante();
+console.log(unMutante.nombre);
+var mifuncion;
+mifuncion = function (num1, num2) { return num1 + num2; };
+console.log(mifuncion(2, 98));
+//clases
+var av2 = new Avenger("hombre hormiga");
+console.log(av2.nombre);
+var Avenger3 = /** @class */ (function () {
+    function Avenger3(nombre) {
+        var _this = this;
+        this._nombre = "un av";
+        this._edad = 23;
+        this.mostrar = function () { return _this._nombre; };
+        this._nombre = nombre;
+    }
+    Object.defineProperty(Avenger3.prototype, "edad", {
+        get: function () { return this._edad; },
+        set: function (e) { this._edad = e; },
+        enumerable: true,
+        configurable: true
+    });
+    return Avenger3;
+}());
+var av3 = new Avenger3("joseEnrique");
+av3.edad = 82;
+console.log(av3.mostrar());
+console.log(av3.edad);
+var Xmen = /** @class */ (function () {
+    function Xmen() {
+    }
+    Xmen.nombreDeClase = "Xmen";
+    return Xmen;
+}());
+console.log(Xmen.nombreDeClase);
+//herencia 
+var AvengerHeredado = /** @class */ (function (_super) {
+    __extends(AvengerHeredado, _super);
+    function AvengerHeredado() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return AvengerHeredado;
+}(Avenger3));
+var avH = new AvengerHeredado("cocacola");
+console.log(avH.mostrar());
+//herencia2
+var AvengerHeredado2 = /** @class */ (function (_super) {
+    __extends(AvengerHeredado2, _super);
+    function AvengerHeredado2(nombre, edad) {
+        var _this = _super.call(this, nombre) || this;
+        _this.edad = 0;
+        _this.edad = edad;
+        return _this;
+    }
+    return AvengerHeredado2;
+}(Avenger3));
+var av5 = new AvengerHeredado2("rigoberto", 23);
+console.log("Avenger " + av5.mostrar() + ", edad: " + av5.edad);
+//namespaces
+var Funciones;
+(function (Funciones) {
+    function f1() {
+        console.log("soy f1");
+    }
+    Funciones.f1 = f1;
+    function f2() {
+        console.log("soy f2");
+    }
+    Funciones.f2 = f2;
+})(Funciones || (Funciones = {}));
+Funciones.f1();
+Funciones.f2();
+$(function () {
+    console.log("ready");
+});
 /// <reference path="hello.ts" />
 var zz;
 //# sourceMappingURL=output.js.map
