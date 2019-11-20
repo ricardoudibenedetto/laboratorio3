9540e424e34d91d5c1f4bcb49b5413da20417e7e
alumno <alumno@utnfra.com.ar>
2019-11-19 22:09:01 -0300

    practica

--- configurador/js/service.js
+++ configurador/js/service.js
@@ -3,6 +3,13 @@ function getAnuncios(){
 }
 
 function guardarAnuncios(anuncio){
- 
-    window.localStorage.setItem('anuncios', JSON.stringify(anuncio));
+    JSON.parse(window.localStorage.getItem("anuncios"))
+    /* si no existe nada en local , agregarle id 1 al elemento  */
+    /* buscar el mayor id max y agregarle uno mas antes de guardar */
+ /*    if(!JSON.parse(window.localStorage.getItem("anuncios"))) {
+        anuncio.id = 1;
+    }else {
+        anuncio.id = 
+    } */
+    window.localStorage.setItem('anuncios', JSON.stringify(anuncios));
 }
